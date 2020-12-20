ee4ea943fc75f273dcae9d44d3ee6bf10940e5f9
DaiJiChen <1245487099@qq.com>
2020-12-20 04:27:14 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -2,6 +2,13 @@
 // Created by Jichen Dai on 12/20/20.
 //
 
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "builtins.h"
 
 int
 cd(size_t argc, char **argv) {
