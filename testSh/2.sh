7d627c2a040a26d5b65873530a472319d0386364
jichendai <jichendai@tencent.com>
2022-09-19 11:19:06 +0800

    learn print params

---
+++ testSh/2.sh
@@ -0,0 +1,7 @@
+my_name="jichendai"
+echo $my_name
+echo ${my_name}
+
+for skill in ada cod ac; do
+	echo "i am good at ${skill} script."
+done
---
+++ testSh/param.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Shell parameter demo";
+echo "file name: $0";
+echo "param1: $1";
+echo "param2: $2";
+echo "param3: $3";
